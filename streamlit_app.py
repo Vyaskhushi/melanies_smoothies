2cc668a0800b2b835c7415fab3aec29683041e58
Vyaskhushi <94962045+Vyaskhushi@users.noreply.github.com>
2025-01-30 12:02:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ if ingredients_list:
     import requests
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
     #st.text(smoothiefroot_response.json())
-    st_df = st.dataframe(data = smoothiefroont_response.json(), use_container_width = True)
+    st_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
     # time_to_insert = st.button('Submit Order')
 
     # if time_to_insert:
